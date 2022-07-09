acf473f091b1ae57a5e852786457dbcb6e664e77
Saqlain1999 <saqlain_shah99@hotmail.com>
2022-07-09 22:45:18 +0500

    Refactored points and maps classes into one file, pointmap.py

---
+++ pointmap.py
@@ -0,0 +1,97 @@
+import pypangolin as pango
+import OpenGL.GL as gl 
+from multiprocessing import Process, Queue
+import numpy as np
+
+
+class Map(object):
+    def __init__(self, Kinv):
+        self.frames = []
+        self.points = []
+        self.Kinv = Kinv
+        self.state = None
+        self.q = Queue()
+        p = Process(target=self.viewer_thread, args=(self.q,))
+        p.daemon = True
+        p.start()
+
+    # Running Threads
+    def viewer_thread(self, q):
+        self.viewer_init(1024, 768)
+        while 1:
+            self.viewer_refresh(q)
+
+    def viewer_init(self, w, h):
+        pango.CreateWindowAndBind("ZlykhSLAM", w, h)
+        gl.glEnable(gl.GL_DEPTH_TEST)
+        
+        self.scam = pango.OpenGlRenderState(
+            pango.ProjectionMatrix(w, h, 420, 420, w//2, h//2, 0.2, 1000),
+            pango.ModelViewLookAt(0, -5, -8,
+                                  0, 0, 0,
+                                  0, -1, 0))
+
+        self.handler = pango.Handler3D(self.scam)
+
+        self.dcam = (
+            pango.CreateDisplay()
+            .SetBounds(
+            pango.Attach(0),
+            pango.Attach(1),
+            pango.Attach(0),
+            pango.Attach(1),
+            -w / h,
+        )
+            .SetHandler(self.handler))
+    def viewer_refresh(self, q):
+        if self.state is None or q.empty():
+            self.state = q.get()
+
+        # turn state into points
+        # ppts = np.array([d[:3, 3] for d in self.state[0]])
+        spts = np.array([d[:3] for d in self.state[1]])
+
+
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+        gl.glClearColor(1.0, 1.0, 1.0, 1.0)
+        self.dcam.Activate(self.scam)
+
+
+        gl.glColor3f(0.0, 1.0, 0.0)
+        for pose in self.state[0]:
+            pango.glDrawFrustum(self.Kinv, 0, 0, pose, 1)
+
+        # pango.glDrawPoints([d[:3,3] for d in self.state[0]])
+
+        gl.glPointSize(2)
+        gl.glColor3f(1.0, 0.0, 0.0)
+        pango.glDrawPoints(spts)
+        
+        pango.FinishFrame()
+        
+
+
+    def display(self):
+        poses, pts = [], []
+        for f in self.frames:
+            poses.append(f.pose)
+        for p in self.points:
+            pts.append(p.pt)
+        self.q.put((poses, pts))
+
+
+class Point(object):
+    # A point is a 3-D point in the world
+    # Each point is observed in multiple Frames
+
+    def __init__(self, mapp, loc):
+        self.pt = loc
+        self.frames = []
+        self.idxs = []
+        
+        self.id = len(mapp.points)
+        mapp.points.append(self)
+
+    def add_observation(self, frame, idx):
+        self.frames.append(frame)
+        self.idxs.append(idx)
--- slam.py
+++ slam.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
+import os
 import time
 import cv2
 import numpy as np
 from display import Display
 from frame import denormalize, Frame, match_frames, IRt
 import g2o
-import pypangolin as pango
-import OpenGL.GL as gl 
-from multiprocessing import Process, Queue
+from pointmap import Map, Point
+
 
 np.set_printoptions(suppress=True)
 
@@ -27,104 +27,9 @@ K = np.array(([F,0,W//2],[0,F,H//2],[0,0,1]))
 Kinv = np.linalg.inv(K)
 
 
-class Map(object):
-    def __init__(self):
-        self.frames = []
-        self.points = []
-        self.state = None
-        self.q = Queue()
-        p = Process(target=self.viewer_thread, args=(self.q,))
-        p.daemon = True
-        p.start()
-
-    # Running Threads
-    def viewer_thread(self, q):
-        self.viewer_init(1024, 768)
-        while 1:
-            self.viewer_refresh(q)
-
-    def viewer_init(self, w, h):
-        pango.CreateWindowAndBind("ZlykhSLAM", w, h)
-        gl.glEnable(gl.GL_DEPTH_TEST)
-        
-        self.scam = pango.OpenGlRenderState(
-            pango.ProjectionMatrix(w, h, 420, 420, w//2, h//2, 0.2, 1000),
-            pango.ModelViewLookAt(0, -5, -8,
-                                  0, 0, 0,
-                                  0, -1, 0))
-
-        self.handler = pango.Handler3D(self.scam)
-
-        self.dcam = (
-            pango.CreateDisplay()
-            .SetBounds(
-            pango.Attach(0),
-            pango.Attach(1),
-            pango.Attach(0),
-            pango.Attach(1),
-            -w / h,
-        )
-            .SetHandler(self.handler))
-    def viewer_refresh(self, q):
-        if self.state is None or q.empty():
-            self.state = q.get()
-
-        # turn state into points
-        # ppts = np.array([d[:3, 3] for d in self.state[0]])
-        spts = np.array([d[:3] for d in self.state[1]])
-
-
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
-        gl.glClearColor(1.0, 1.0, 1.0, 1.0)
-        self.dcam.Activate(self.scam)
-
-
-        gl.glColor3f(0.0, 1.0, 0.0)
-        for pose in self.state[0]:
-            pango.glDrawFrustum(Kinv, 0, 0, pose, 1)
-
-        # pango.glDrawPoints([d[:3,3] for d in self.state[0]])
-
-        gl.glPointSize(2)
-        gl.glColor3f(1.0, 0.0, 0.0)
-        pango.glDrawPoints(spts)
-        
-        pango.FinishFrame()
-        
-
-
-    def display(self):
-        poses, pts = [], []
-        for f in self.frames:
-            poses.append(f.pose)
-        for p in self.points:
-            pts.append(p.pt)
-        self.q.put((poses, pts))
-
 # main classes
-mapp = Map()
-# display = Display(W,H)
-display = None
-
-
-    # for p in points:
-    #     print(p.xyz)
-
-class Point(object):
-    # A point is a 3-D point in the world
-    # Each point is observed in multiple Frames
-
-    def __init__(self, mapp, loc):
-        self.pt = loc
-        self.frames = []
-        self.idxs = []
-        
-        self.id = len(mapp.points)
-        mapp.points.append(self)
-
-    def add_observation(self, frame, idx):
-        self.frames.append(frame)
-        self.idxs.append(idx)
+mapp = Map(Kinv)
+display = Display(W,H) if os.getenv("D2D") is not None else None
 
 def triangulate(pose1, pose2, pts1, pts2):
     return cv2.triangulatePoints(pose1[:3], pose2[:3], pts1.T, pts2.T).T
